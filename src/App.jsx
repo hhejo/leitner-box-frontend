ffcf8d3b6ecb9eeba18bd62f04839be9c9598e60
hhejo <litsmainjof@gmail.com>
2023-12-14 22:30:42 +0900

    feat: data fetch success

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Form from "./Form";
 import CardList from "./CardList";
 import "./App.css";
 
+const URL = "http://localhost:3000";
+
 function App() {
   const [cardList, setCardList] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(URL, { method: "GET" });
+      const json = await res.json();
+      console.log(json);
+    })();
+  }, []);
   const submitHandler = ({ question, answer }) => {
     setCardList((prev) => {
       return [...prev, { question, answer }];
     });
+    (async () => {
+      const res = await fetch(`${URL}/cards`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" }, // 안 쓰면 백엔드에서 {} 로 나옴
+        body: JSON.stringify({
+          question,
+          answer,
+        }),
+      });
+      console.log(res);
+    })();
   };
   return (
     <>

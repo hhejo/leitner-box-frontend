1e7a6007f246608c0f127a70e960fdd72a2cf52a
hhejo <litsmainjof@gmail.com>
2024-01-01 21:07:21 +0900

    feat: axios 설치, 카드 생성, 삭제 핸들러 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 import Form from "./Form";
 import CardList from "./CardList";
 import "./App.css";
@@ -7,33 +8,31 @@ const URL = "http://localhost:3000";
 
 function App() {
   const [cardList, setCardList] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const res = await fetch(URL, { method: "GET" });
-      const json = await res.json();
-      console.log(json);
-    })();
-  }, []);
-  const submitHandler = ({ question, answer }) => {
-    setCardList((prev) => {
-      return [...prev, { question, answer }];
-    });
-    (async () => {
-      const res = await fetch(`${URL}/cards`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" }, // 안 쓰면 백엔드에서 {} 로 나옴
-        body: JSON.stringify({
-          question,
-          answer,
-        }),
-      });
-      console.log(res);
-    })();
+
+  const getCards = async () => {
+    const { data: cards } = await axios.get(URL);
+    setCardList(cards);
   };
+
+  useEffect(() => {
+    getCards();
+  }, []);
+
+  const submitHandler = async ({ question, answer }) => {
+    const newCard = { question, answer };
+    await axios.post(`${URL}/cards`, newCard);
+    getCards();
+  };
+
+  const deleteHandler = async (cardId) => {
+    await axios.delete(`${URL}/cards/${cardId}`);
+    getCards();
+  };
+
   return (
     <>
       <Form onSubmit={submitHandler} />
-      <CardList cardList={cardList} />
+      <CardList cardList={cardList} onDelete={deleteHandler} />
     </>
   );
 }
